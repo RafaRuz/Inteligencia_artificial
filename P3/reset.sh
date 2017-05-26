6840350a1ebd2c2c24b2cf952d37f19ec7d253e4
rafu96 <rafaroco96@gmail.com>
2017-05-26 18:11:42 +0200

    P3

---
+++ P3/reset.sh
@@ -0,0 +1,9 @@
+rm -fr ./bots/mybot/maps/*
+rm -fr ./bots/mybot/sets/*
+touch ./bots/mybot/sets/contactos.txt
+touch ./bots/mybot/maps/nombres.txt
+touch ./bots/mybot/maps/direcciones.txt
+touch ./bots/mybot/maps/telefonos.txt
+touch ./bots/mybot/maps/eventos.txt
+touch ./bots/mybot/maps/evper.txt
+cp ./bots/mybot/hora_posicion.txt ./bots/mybot/maps/
